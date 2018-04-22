f4760de4f69ebe25c4b79bf45e9e10435457b9ea
Brendan Doncaster <brendan.doncaster@connect.qut.edu.au>
2018-04-22 11:53:29 +1000

    Completed BruteForceMedian method within BruteForceAlgorithm and created TestImplementation with basic test for BruteForce

--- src/Assignment/BruteForceAlgorithm.java
+++ src/Assignment/BruteForceAlgorithm.java
@@ -2,8 +2,30 @@ package Assignment;
 
 public class BruteForceAlgorithm {
 
-	public BruteForceAlgorithm() {
-		// TODO Auto-generated constructor stub
-	}
-
+	public static int BruteForceMedian(int[] list) {
+		int k = (int) Math.ceil(list.length/2.0);
+		
+		for(int i = 0; i < list.length-1; i++) {
+			int numSmaller = 0;
+			int numEqual = 0;
+			
+			for(int j = 0; j < list.length-1; j++) {
+				
+				if(list[j] < list[i]) {
+					numSmaller++;
+				}
+				else if(list[j] == list[i]) {
+					numEqual++;
+				}				
+			}			
+			if(numSmaller < k && k <= (numSmaller + numEqual)) {
+				return list[i];
+			}			
+		}		
+		//in case of no median found return 0
+		return 0;
+	} //end BruteForceMedian
+	
+	
+	
 }
---
+++ src/Assignment/TestImplementaion.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+public class TestImplementaion {
+
+	public static void main(String[] args) {
+		
+		//example list of numbers (median is 8)
+		int[] test = {4,1,10,9,7,12,8,2,15};
+		
+		System.out.println("Median = " + BruteForceAlgorithm.BruteForceMedian(test));
+
+	}
+
+}
