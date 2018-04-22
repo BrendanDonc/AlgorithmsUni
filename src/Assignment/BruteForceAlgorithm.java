1f6175ab14ddde8fe4683d4a599521602bc3a4bd
Brendan Doncaster <brendan.doncaster@connect.qut.edu.au>
2018-04-22 19:39:36 +1000

    Fixed BruteForce and added extra test cases

--- src/Assignment/BruteForceAlgorithm.java
+++ src/Assignment/BruteForceAlgorithm.java
@@ -5,11 +5,11 @@ public class BruteForceAlgorithm {
 	public static int BruteForceMedian(int[] list) {
 		int k = (int) Math.ceil(list.length/2.0);
 		
-		for(int i = 0; i < list.length-1; i++) {
+		for(int i = 0; i < list.length; i++) {
 			int numSmaller = 0;
 			int numEqual = 0;
 			
-			for(int j = 0; j < list.length-1; j++) {
+			for(int j = 0; j < list.length; j++) {
 				
 				if(list[j] < list[i]) {
 					numSmaller++;
