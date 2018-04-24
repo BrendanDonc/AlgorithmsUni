e4ecc79c648d8c6ef2a64fd9af09422e3fb3c5d9
Brendan Doncaster <brendan.doncaster@connect.qut.edu.au>
2018-04-24 20:31:49 +1000

    Added BruteForceMedianOps to BruteForceAlgorithm

--- src/Assignment/BruteForceAlgorithm.java
+++ src/Assignment/BruteForceAlgorithm.java
@@ -26,6 +26,35 @@ public class BruteForceAlgorithm {
 		return 0;
 	} //end BruteForceMedian
 	
-	
+	public static int BruteForceMedianOps(int[] list) {
+		int k = (int) Math.ceil(list.length/2.0);
+		int ops = 0;
+		
+		for(int i = 0; i < list.length; i++) {
+			int numSmaller = 0;
+			int numEqual = 0;
+			
+			for(int j = 0; j < list.length; j++) {
+				
+				//add the two possible operations that occur when if statement fails
+				ops = ops + 2;
+				
+				if(list[j] < list[i]) {
+					numSmaller++;
+					
+					//remove the one operation that didn't occur since if statement passed
+					ops = ops - 1;
+				}
+				else if(list[j] == list[i]) {
+					numEqual++;
+				}				
+			}			
+			if(numSmaller < k && k <= (numSmaller + numEqual)) {
+				return ops;
+			}			
+		}		
+		//in case of no median found return 0
+		return ops;
+	} //end BruteForceMedianOps
 	
 }
