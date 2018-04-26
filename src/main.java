e13c05d8c857fdd6e6fb55691f5d1557e693d8c8
Brendan Doncaster <brendan.doncaster@connect.qut.edu.au>
2018-04-26 15:29:12 +1000

    Test stuff for bruteforce

--- src/main.java
+++ src/main.java
@@ -11,9 +11,9 @@ public class main {
         int[] test3 = {0, 0, 1, 1, 7, 8, 9, 16, 17, 20, 23, 37, 38, 40, 41, 45, 55, 61, 64, 67, 72, 81, 82, 95, 98};
         int[] test4 = {1, 1, 1, 1, 1, 1, 1, 1, 5, 9, 9, 9, 9, 9, 9, 9, 9};
         int[] test5 = {9, 9, 9, 9, 9, 9, 9, 9, 5, 1, 1, 1, 1, 1, 1, 1, 1}; 
-        int[] test6 = {82, 41, 41, 37, 81};
+        int[] test6 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        int[] currentTest = test2;//Just so we only have to change it in one place
+        int[] currentTest = test6;//Just so we only have to change it in one place
 
         System.out.println("Brute force median = " + BruteForceAlgorithm.BruteForceMedian(currentTest));
         System.out.println("Partition median = " + PartitionAlgorithm.Median(currentTest));
