7eb36b629e2add6d34f1bb38f30a34ab10250a8e
Rick_Mccasker <rickmccasker@gmail.com>
2018-05-10 16:44:19 +1000

    Cleanup partition

--- src/Assignment/PartitionAlgorithm.java
+++ src/Assignment/PartitionAlgorithm.java
@@ -2,7 +2,6 @@ package Assignment;
 
 public class PartitionAlgorithm {
     public static int Median(int[] array){
-        //Exit early if array only contains one element
         if(array.length == 1){
             return array[0];
         }
@@ -11,38 +10,27 @@ public class PartitionAlgorithm {
         }
     }//end Median
 
-    //Recursively run until the median index matches as expected
     public static int Select(int[] array, int min, int median_index, int max){
-
-        //Try to assign pos the median index from a partially sorted array
         int pos = Partition(array, min, max);
 
-        //If the median index is as expected
         if(pos == median_index){
             return array[pos];
         }
-        //Reduce array slice from top if correct index exists below
         if(pos > median_index){
-            //System.out.println(pos + " compares to > " + median_index);
             return Select(array, min, median_index, pos-1);
         }
-        //Reduce array slice from bottom if correct index exists above
         if(pos < median_index){
-            //System.out.println(pos + " compares to <" + median_index);
             return Select(array, pos+1, median_index, max);
         }
         return 0;
     }//end Select
 
     public static int Partition(int[] array, int min, int max){
-        //Record first val in array slice and position
-        int pivot_val = array[min]; //Pivot point to be used
+        int pivot_val = array[min];
         int pivot_loc = min;
-        //Iterate over slice excluding first val
         for(int j = min + 1; j <= max; j++){
-            //Check if the current val index is less that the recorded pivot point
             if(array[j] < pivot_val){
-                pivot_loc++; //Iterate pivot loc to match movement
+                pivot_loc++;
                 array = swap(array, pivot_loc, j);
             }
         }
@@ -64,61 +52,41 @@ public class PartitionAlgorithm {
 //-------------------------------------
     public static int op_counter;
     public static int MedianOps(int[] array){
-        //Exit early if array only contains one element
         op_counter = 0;
         if(array.length == 1){
             return op_counter;
         }
         else{
             SelectOps(array, 0, (int) Math.floor(array.length/2), array.length-1);
-
-            //System.out.println(op_counter);
             return op_counter;
         }
     }//end Median
 
-    //Recursively run until the median index matches as expected
     public static int SelectOps(int[] array, int min, int median_index, int max){
-        //Try to assign pos the median index from a partially sorted array
         int pos = PartitionOps(array, min, max);
-        //If the median index is as expected
         if(pos == median_index){
             return array[pos];
         }
-        //Reduce array slice from top if correct index exists below
         if(pos > median_index){
-            //System.out.println(pos + " compares to > " + median_index);
             return SelectOps(array, min, median_index, pos-1);
         }
-        //Reduce array slice from bottom if correct index exists above
         if(pos < median_index){
-            //System.out.println(pos + " compares to <" + median_index);
             return SelectOps(array, pos+1, median_index, max);
         }
         return 0;
     }//end Select
 
     public static int PartitionOps(int[] array, int min, int max){
-        //Record first val in array slice and position
-        int pivot_val = array[min]; //Pivot point to be used
+        int pivot_val = array[min];
         int pivot_loc = min;
-        //Iterate over slice excluding first val
         for(int j = min + 1; j <= max; j++){
-            //Check if the current val index is less that the recorded pivot point
             op_counter += 1;
             if(array[j] < pivot_val){
-                pivot_loc++; //Iterate pivot loc to match movement
-                array = swapOps(array, pivot_loc, j); //dont need array equals prolly, delete this
+                pivot_loc++;
+                array = swap(array, pivot_loc, j);
             }
         }
-        array = swapOps(array, min, pivot_loc);
+        array = swap(array, min, pivot_loc);
         return pivot_loc;
     }//end Partition
-
-    public static int[] swapOps(int[] array, int a, int b){
-        int temp = array[a];
-        array[a] = array[b];
-        array[b] = temp;
-        return array;
-    }//end swap
 }
